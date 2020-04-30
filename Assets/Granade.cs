bd5c63dda66bd3e1fad0a5cc8e1b3d56719d4821
Mikołaj Nawrocki <m.nawrocki793@gmail.com>
2020-04-30 21:50:06 +0200

    1. PP02_P1_PIERWSZY_SKRYPT

---
+++ Assets/Granade.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Granade : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (tag=="Enemy")
+        {
+            ///destroy,getdamage,vfx,sfx
+        }
+    }
+
+}
