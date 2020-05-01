2af3b71a2ad77f3e9940743e4990a8739597c5b0
Mikołaj Nawrocki <m.nawrocki793@gmail.com>
2020-05-01 11:36:00 +0200

    PP02_P2_PREFAB_GRANATU

---
+++ Assets/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] int health = 100;
+    [SerializeField] int damageAmount = 5;
+    [SerializeField] Transform granadePosition;
+
+    bool isAlive = true;
+
+    Granade granade;
+
+    private void Start()
+    {
+        granade = FindObjectOfType<Granade>();
+    }
+
+    public void TakeDamage()
+    {
+        if (isAlive)
+        {
+            float distanceToGranade = Vector3.Distance(transform.position, granadePosition.position);
+            if (distanceToGranade < granade.explosionRadius)
+            {
+                health -= 10;
+
+            }
+        }
+    }
+
+
+
+}
--- Assets/Granade.cs
+++ Assets/Granade.cs
@@ -1,16 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Granade : MonoBehaviour
 {
+    [SerializeField] GameObject explosion;
+    public float explosionRadius = 5f;
 
-    private void OnCollisionEnter(Collision collision)
+
+    Enemy enemy;
+
+    private void Start()
     {
-        if (tag=="Enemy")
+        enemy = FindObjectOfType<Enemy>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
         {
-            ///destroy,getdamage,vfx,sfx
+            ThrowGranade();
         }
     }
 
+    public void ThrowGranade()
+    {
+        StartCoroutine(ExplosionTimer());
+        Destroy(gameObject, 2.1f);
+    }
+
+    private void HurtEnemy()
+    {
+        if (enemy.CompareTag("Enemy"))
+        {
+            enemy.TakeDamage();
+        }
+    }
+
+    IEnumerator ExplosionTimer()
+    {
+        yield return new WaitForSeconds(2f);
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        HurtEnemy();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
 }
