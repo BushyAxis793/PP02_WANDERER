f136bf5a79d5e2da782a7d8f9ecb73fe702391a0
Mikołaj Nawrocki <m.nawrocki793@gmail.com>
2020-05-01 17:25:10 +0200

    PP02_P3_PROGRAMOWANIE_POSTACI

---
+++ Assets/Player.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float movementSpeed;
+    float walkSpeed = 10f;
+    float crouchSpeed = 5f;
+    float runSpeed = 15f;
+
+
+    Rigidbody myRigidbody;
+
+    bool isCrouching = false;
+    bool isRunning = false;
+    bool isAlive = true;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        movementSpeed = 10f;
+    }
+
+
+    void Update()
+    {
+        if (isAlive)
+        {
+            PlayerMovement();
+            PlayerCrouch();
+            PlayerRun();
+        }
+        else
+        {
+            Die();
+        }
+        print(movementSpeed);
+    }
+
+    private void Die()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void PlayerRun()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
+        {
+            isRunning = true;
+            movementSpeed = runSpeed;
+        }
+        else
+        {
+            isRunning = false;
+            movementSpeed = walkSpeed;
+        }
+        ////todo uniezaleznic bieg od kucania
+
+    }
+
+    private void PlayerCrouch()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            isCrouching = true;
+
+            if (isCrouching)
+            {
+                isRunning = false;
+                Vector3 crouchPos = new Vector3(transform.position.x, transform.position.y - 1f, transform.position.z);
+                transform.position = crouchPos;
+                movementSpeed = crouchSpeed;
+
+            }
+            else
+            {
+                Vector3 nonCrouchPos = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
+                transform.position = nonCrouchPos;
+                movementSpeed = walkSpeed;
+
+            }
+        }
+        else
+        {
+            isCrouching = false;
+        }
+    }
+
+    private void PlayerMovement()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * movementSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * movementSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+        }
+    }
+}
