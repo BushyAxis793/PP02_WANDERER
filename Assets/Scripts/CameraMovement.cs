9e9bb01cd395abe4da0c3eac28c66ea869b103ba
Mikołaj Nawrocki <m.nawrocki793@gmail.com>
2020-05-03 17:37:47 +0200

    PP02_P7_POPRAWKI (PARTICLE SYSTEM, MOVEMENT)

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    private float rotationX, rotationY;
+
+    private float minX = -360f;
+    private float maxX = 360f;
+
+    private float minY = -60f;
+    private float maxY = 80f;
+
+    private Quaternion baseRotation;
+
+
+
+    void Start()
+    {
+        baseRotation = transform.rotation;
+    }
+
+    void LateUpdate()
+    {
+        RotationHandle();
+    }
+
+    float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360f)
+        {
+            angle += 360f;
+        }
+        if (angle > 360f)
+        {
+            angle -= 360f;
+        }
+        return Mathf.Clamp(angle, min, max);
+    }
+
+    void RotationHandle()
+    {
+        Quaternion xRotation = CalcAxisX();
+        Quaternion yRotation = CalcAxisY();
+        transform.rotation = baseRotation * xRotation * yRotation;
+
+    }
+
+    private Quaternion CalcAxisY()
+    {
+        float sensivityY = 1.5f;
+        rotationY += Input.GetAxis("Mouse Y") * sensivityY;
+        rotationY = ClampAngle(rotationY, minY, maxY);
+        Quaternion yRotation = Quaternion.AngleAxis(rotationY, Vector3.left);
+        return yRotation;
+    }
+
+    public Quaternion CalcAxisX()
+    {
+        float sensivityX = 1.5f;
+        rotationX += Input.GetAxis("Mouse X") * sensivityX;
+        rotationX = ClampAngle(rotationX, minX, maxX);
+        Quaternion xRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+        return xRotation;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float movementSpeed;
+    float walkSpeed = 5f;
+    float crouchSpeed = 2f;
+    float runSpeed = 10f;
+    float jumpSpeed = 5f;
+    float inputY;
+    float inputX;
+
+    Rigidbody myRigidbody;
+    CharacterController charController;
+    Quaternion baseRotation;
+
+    bool isCrouching = false;
+    bool isRunning = false;
+    bool isAlive = true;
+    bool isGrounded = true;
+
+
+    void Start()
+    {
+
+        myRigidbody = GetComponent<Rigidbody>();
+        charController = GetComponent<CharacterController>();
+        movementSpeed = 10f;
+        transform.rotation = baseRotation;
+    }
+
+
+    void Update()
+    {
+        if (isAlive)
+        {
+            PlayerMovement();
+            PlayerCrouch();
+            PlayerRun();
+            PlayerJump();
+        }
+        else
+        {
+            Die();
+        }
+        print(movementSpeed);
+
+    }
+
+    private void Die()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void PlayerJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            if (!isCrouching)
+            {
+                myRigidbody.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+            }
+        }
+    }
+
+    private void PlayerRun()
+    {
+        if (!isCrouching)
+        {
+            if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
+            {
+                isRunning = true;
+                movementSpeed = runSpeed;
+            }
+            else
+            {
+                isRunning = false;
+                movementSpeed = walkSpeed;
+            }
+        }
+
+    }
+
+    private void PlayerCrouch()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (isCrouching)
+            {
+                isCrouching = false;
+            }
+            else
+            {
+                isCrouching = true;
+            }
+
+            if (isCrouching)
+            {
+                Vector3 crouchPos = new Vector3(transform.position.x, transform.position.y - .5f, transform.position.z);
+                Camera.main.transform.position = crouchPos;
+                movementSpeed = crouchSpeed;
+            }
+            else
+            {
+                Vector3 nonCrouchPos = new Vector3(transform.position.x, transform.position.y + .5f, transform.position.z);
+                Camera.main.transform.position = nonCrouchPos;
+                movementSpeed = walkSpeed;
+            }
+        }
+
+
+    }
+
+    private void PlayerMovement()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+
+        Vector3 moveVector = transform.right * inputX + transform.forward * inputZ;
+        charController.Move(moveVector * movementSpeed * Time.deltaTime);
+        //todo poprawic poruszanie się
+
+    }
+
+
+
+
+
+}
